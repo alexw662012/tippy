fda4f657ffefe1d2e3a60175c2c1ddcc8b1da3c8
Alex Wang <awang@saicusa.com>
2017-03-01 23:11:07 -0800

    this is the first app in codepath class

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -10,9 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var SeperatorView: UIView!
+    @IBOutlet weak var TipControl: UISegmentedControl!
+    @IBOutlet weak var BillField: UITextField!
+    @IBOutlet weak var TotalLabel: UILabel!
+    @IBOutlet weak var TipLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("view did load")
+      
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+        let defaults = UserDefaults.standard
+        let defaultTip = defaults.object(forKey: "default_tips") as? Int
+        
+        TipControl.selectedSegmentIndex = defaultTip ?? 0
+        TipControl.sendActions(for: UIControlEvents.valueChanged)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.SeperatorView.alpha = 0
+        print("view did appear")
+        UIView.animate(withDuration: 2, animations: {
+            self.SeperatorView.alpha = 1
+        })
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("view did disappear")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +55,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
 
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        let tipPercentage = [0.15, 0.18, 0.2]
+        let bill = Double(BillField.text!) ?? 0
+        let tip = bill * tipPercentage[TipControl.selectedSegmentIndex]
+        let total = bill + tip
+        TipLabel.text = String(format: "$%.2f", tip)
+        TotalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
